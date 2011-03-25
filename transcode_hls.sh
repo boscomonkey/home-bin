09d5f58c44a3c20c99c7b559fccdef3b1e8a3204
Orange Bosco <bosco.so@orange-ftgroup.com>
2011-03-25 15:47:15 -0700

    added "-threads 0" param to take advantage of multi-core CPUs

--- transcode_hls.sh
+++ transcode_hls.sh
@@ -6,4 +6,4 @@
 BITRATE=800k
 test -n "$3" && BITRATE="$3"
 
-ffmpeg -i "$1" -f mpegts -acodec libmp3lame -ar 48000 -ab 64k -vcodec libx264 -b "${BITRATE}" -flags +loop -cmp +chroma -partitions +parti4x4+partp8x8+partb8x8 -subq 5 -trellis 1 -refs 1 -coder 0 -me_range 16 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -bt 200k -rc_eq 'blurCplx^(1-qComp)' -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -level 30 -g 30 -async 2 -r 24 "$2"
+ffmpeg -i "$1" -f mpegts -acodec libmp3lame -ar 48000 -ab 64k -vcodec libx264 -b "${BITRATE}" -flags +loop -cmp +chroma -partitions +parti4x4+partp8x8+partb8x8 -subq 5 -trellis 1 -refs 1 -coder 0 -me_range 16 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -bt 200k -rc_eq 'blurCplx^(1-qComp)' -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -level 30 -g 30 -async 2 -r 24 -threads 0 "$2"
