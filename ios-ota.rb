d4b8a780212a0cf14ec811d5fd49591717e00764
Bosco So <rubymeetup@boscoso.com>
2011-09-26 13:00:02 -0700

    use jQuery Mobile for HTML output - displays nicer on IOS devices

--- ios-ota.rb
+++ ios-ota.rb
@@ -31,45 +31,54 @@ plist_url = "#{base_url}/#{plist_fname}"
 readme_url = "#{base_url}/readme.html"
 
 print <<EOF
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<!DOCTYPE html>
 <html>
   <head>
     <title>#{title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.css" />
+    <script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.0b3/jquery.mobile-1.0b3.min.js"></script>
     <style>
-      body {
-      font-family: Verdana, Helvetica, sans-serif;
-      font-size: smaller;
-      }
     </style>
   </head>
 
   <body>
-    <h1>#{title}</h1>
+    <div data-role="page" id="splash" data-title="#{title}">
+      <div data-role="header">
+        <h1>#{title}</h1>
+      </div>
 
-   <p>
-      In order to install this app on your IOS 4.2 device, click the
-      two links below in your IOS Safari browser:
-    </p>
+      <div data-role="content">
+        <p>
+          In order to install this app on your IOS 4.2 (or later)
+          device, click the two buttons below in the order in which
+          they appear.
+        </p>
 
-    <ol>
-      <li>
-	<a href="#{provision_url}">
-	Install Provisioning File</a>
-      </li>
-      <li>
-	<a href="itms-services://?action=download-manifest&url=#{plist_url}">
-	Install Application</a>
-      </li>
-    </ol>
+        <p>
+          The first button will take you away from your web
+          browser. Remember to return to your browser afterward to
+          finish the installation.
+        </p>
 
-    <hr>
-    <address>
-      <p>
-	The URL for this page
-	is <a href="#{readme_url}">#{readme_url}</a>
-      </p>
-    </address>
-    <!-- hhmts start --> Last modified: #{Time.now} <!-- hhmts end -->
+        <!--
+                NOTE: bit.ly the provisioning file URL
+        -->
+        <a href="#{provision_url}"
+           data-role="button" data-icon="arrow-r"
+           data-iconpos="right">Install Provisioning File</a>
+        <a href="itms-services://?action=download-manifest&url=#{plist_url}"
+              data-role="button" data-icon="arrow-r"
+              data-iconpos="right">Install Application</a>
+      </div>
+
+      <div data-role="footer">
+        <h4>#{Time.now}</h4>
+      </div>
+    </div>
+
   </body>
 </html>
 EOF
