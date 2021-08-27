c021f7bd818f0abffa4a1e050260d4f9e91cbdd0
Bosco So <bosco.so@hingehealth.com>
2021-08-27 11:28:20 -0700

    temp json values are saved as in-memory variables - thus no temp files are left behind

--- jsonify_env_file.sh
+++ jsonify_env_file.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+# Converts a dotenv file to JSON. This script is capable of parsing
+# multiline values, as well as ignoring comments. The dotenv file must
+# be specified as the 1st argument - STDIN input is not supported.
+#
+# Useful for parsing output of `aptible config`.
+
+# converts env vars to JSON
+CMD="ruby -r json -e 'puts JSON.pretty_generate(ENV.keys.sort.map { |key| [key, ENV[key]] }.to_h)'"
+
+# first arg names envfile
 ENVFILE="$1"
 if [ -z "$ENVFILE" ]; then
     echo "USAGE: $0 DOTENV_FILE" > /dev/stderr
     exit 1
 fi
 
-BAREBONES_VARS_FNAME=barebones.json
-ENV_JSON_CMD="ruby -r json -e 'puts JSON.pretty_generate(ENV.keys.sort.map { |key| [key, ENV[key]] }.to_h)'"
-ENVFILE_VARS_FNAME=envfile.json
+# jsonify barebones env vars
+barebones_str=$(env -i bash -c "$CMD")
 
-# jsonify Bash barebones env vars
-env -i bash -c "$ENV_JSON_CMD" > $BAREBONES_VARS_FNAME
-
-# jsonify Bash envfile env vars
-env -i bash -c "set -a; . ${ENVFILE}; set +a; ${ENV_JSON_CMD}" > $ENVFILE_VARS_FNAME
+# jsonify envfile env vars
+envfile_str=$(env -i bash -c "set -a; . ${ENVFILE}; set +a; ${CMD}")
 
 # subtract first json from second
 ruby -r json -e "
-     barebones = JSON.parse(File.read('$BAREBONES_VARS_FNAME'))
-     envfile = JSON.parse(File.read('$ENVFILE_VARS_FNAME'))
+     barebones = JSON.parse(ARGV[0])
+     envfile = JSON.parse(ARGV[1])
      diff_keys = envfile.keys - barebones.keys
      puts JSON.pretty_generate(diff_keys.sort.map { |key| [key, envfile[key]] }.to_h)
-"
+" \
+     "$barebones_str" \
+     "$envfile_str"
